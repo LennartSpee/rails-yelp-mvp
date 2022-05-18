ca1f3b2e5382a37121735a517eb5fb7c0ab6ed40
Lennart Spee <lennart@digitallokal.de>
2022-05-18 13:01:24 +0100

    rails yelp

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    collection do
+      get :top
+      get :worst
+    end
+    member do
+      get :chef
+    end
+    resources :reviews, only: [:new, :create]
+  end
+  resources :reviews, only: :destroy
+  # get 'restaurants/top', to: 'restaurants#top'
+  # get 'restaurants/:id/chef', to: 'restaurants#chef'
 end
